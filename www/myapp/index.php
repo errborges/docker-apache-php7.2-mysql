cf6b9199c6750692c31ffe466df2ba6b3069a5e3
borges <borges@borges.com>
2024-05-21 22:24:09 -0300

    exemplo

--- www/myapp/index.php
+++ www/myapp/index.php
@@ -1,3 +1,12 @@
 <?php
 
-phpinfo();
+//phpinfo();
+//$conn = mysqli_connect("db", "root", "root", "eborges") or die(mysqli_error());
+//echo 'Banco conectado';
+//$conn->close();
+
+$banco = new PDO('mysql:host=db;dbname=eborges', 'root','root');
+print "Conexão Efetuada com sucesso!";
+
+$novo_cliente = array('nome'=>'Edson','departamento'=>'TI','unidade'=>'Brasil');
+$banco->prepare('INSERT INTO clientes (nome,departamento,unidade) VALUES (:nome,:departamento,:unidade)')->execute($novo_cliente);
